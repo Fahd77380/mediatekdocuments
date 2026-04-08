955328ab3e7824325206571eadc45a8f878a9644
Fahd77380 <zahoumfahd@gmail.com>
2026-04-08 17:30:57 +0200

    Envoi final propre

---
+++ MediaTekDocuments/Program.cs
@@ -0,0 +1,24 @@
+﻿using MediaTekDocuments.view;
+using System;
+using System.Windows.Forms;
+
+namespace MediaTekDocuments
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            FrmLogin frmLogin = new FrmLogin();
+            if (frmLogin.ShowDialog() == DialogResult.OK)
+            {
+                Application.Run(new FrmMediatek(frmLogin.UtilisateurConnecte));
+            }
+        }
+    }
+}
---
+++ MediaTekDocuments/controller/FrmMediatekController.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+using MediaTekDocuments.model;
+using MediaTekDocuments.dal;
+
+namespace MediaTekDocuments.controller
+{
+    /// <summary>
+    /// Contrôleur lié à FrmMediatek
+    /// </summary>
+    class FrmMediatekController
+    {
+        /// <summary>
+        /// Objet d'accès aux données
+        /// </summary>
+        private readonly Access access;
+
+        /// <summary>
+        /// Récupération de l'instance unique d'accès aux données
+        /// </summary>
+        public FrmMediatekController()
+        {
+            access = Access.GetInstance();
+        }
+
+        /// <summary>
+        /// getter sur la liste des genres
+        /// </summary>
+        /// <returns>Liste d'objets Genre</returns>
+        public List<Categorie> GetAllGenres()
+        {
+            return access.GetAllGenres();
+        }
+
+        /// <summary>
+        /// getter sur la liste des livres
+        /// </summary>
+        /// <returns>Liste d'objets Livre</returns>
+        public List<Livre> GetAllLivres()
+        {
+            return access.GetAllLivres();
+        }
+
+        /// <summary>
+        /// getter sur la liste des Dvd
+        /// </summary>
+        /// <returns>Liste d'objets dvd</returns>
+        public List<Dvd> GetAllDvd()
+        {
+            return access.GetAllDvd();
+        }
+
+        /// <summary>
+        /// getter sur la liste des revues
+        /// </summary>
+        /// <returns>Liste d'objets Revue</returns>
+        public List<Revue> GetAllRevues()
+        {
+            return access.GetAllRevues();
+        }
+
+        /// <summary>
+        /// getter sur les rayons
+        /// </summary>
+        /// <returns>Liste d'objets Rayon</returns>
+        public List<Categorie> GetAllRayons()
+        {
+            return access.GetAllRayons();
+        }
+
+        /// <summary>
+        /// getter sur les publics
+        /// </summary>
+        /// <returns>Liste d'objets Public</returns>
+        public List<Categorie> GetAllPublics()
+        {
+            return access.GetAllPublics();
+        }
+
+
+        /// <summary>
+        /// récupère les exemplaires d'une revue
+        /// </summary>
+        /// <param name="idDocument">id de la revue concernée</param>
+        /// <returns>Liste d'objets Exemplaire</returns>
+        public List<Exemplaire> GetExemplairesRevue(string idDocument)
+        {
+            return access.GetExemplairesRevue(idDocument);
+        }
+
+        /// <summary>
+        /// Crée un exemplaire d'une revue dans la bdd
+        /// </summary>
+        /// <param name="exemplaire">L'objet Exemplaire concerné</param>
+        /// <returns>True si la création a pu se faire</returns>
+        public bool CreerExemplaire(Exemplaire exemplaire)
+        {
+            return access.CreerExemplaire(exemplaire);
+        }
+
+        public List<CommandeDocument> GetCommandesDocument(string idLivreDvd)
+        {
+            return access.GetCommandesDocument(idLivreDvd);
+        }
+
+        public List<Suivi> GetAllSuivi()
+        {
+            return access.GetAllSuivi();
+        }
+
+        public bool CreerCommandeDocument(CommandeDocument commande)
+        {
+            return access.CreerCommandeDocument(commande);
+        }
+
+        public bool UpdateSuiviCommande(string idCommande, int idSuivi)
+        {
+            return access.UpdateSuiviCommande(idCommande, idSuivi);
+        }
+
+        public bool SupprimerCommandeDocument(string idCommande)
+        {
+            return access.SupprimerCommandeDocument(idCommande);
+        }
+
+        public List<Abonnement> GetAbonnementsRevue(string idRevue)
+        {
+            return access.GetAbonnementsRevue(idRevue);
+        }
+
+        public bool CreerAbonnement(Abonnement abonnement)
+        {
+            return access.CreerAbonnement(abonnement);
+        }
+
+        public bool SupprimerAbonnement(string idAbonnement)
+        {
+            return access.SupprimerAbonnement(idAbonnement);
+        }
+
+        public List<Abonnement> GetAbonnementsExpiration()
+        {
+            return access.GetAbonnementsExpiration();
+        }
+    }
+}
